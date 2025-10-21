7f1c8ce807d0ddaad2487a4068f9304560809f2f
Ashley Miltenberger <ashley.miltenberger@csuglobal.edu>
2025-10-21 18:27:51 +0000

    Add Dockerfile, entrypoint script, and action configuration for podcast generator

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "==============================="
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+python3 /urs/bin/update_feed.py
+
+git ad -A && git commit -m "Update feed"
+
+git push --set-upstream origin main
+
+
+
+echo "==============================="
+
+
